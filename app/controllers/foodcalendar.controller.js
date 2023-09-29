8745f7982a8c5256495d9e317aa36f9de58065db
Tatsi <tase64@gmail.com>
2023-09-29 17:37:41 +0300

    mofidied

--- app/controllers/foodcalendar.controller.js
+++ app/controllers/foodcalendar.controller.js
@@ -3,12 +3,11 @@ const FoodCalendar = db.foodcalendar
 const Op = db.Sequelize.Op
 
 module.exports.findAll = (req, res) => {
-
     FoodCalendar.findAll({
         include: [{
             association: 'CalendarItems',
             required: false,
-          }],
+        }]
     })
         .then(data => {
             res.send(data);
@@ -20,6 +19,17 @@ module.exports.findAll = (req, res) => {
 }
 
 module.exports.findById = (req, res) => {
-
+    const id = req.params.id;
+    FoodCalendar.findByPk(id, {
+        include: [{
+            association: 'CalendarItems',
+            required: false,
+        }]
+    })
+        .then(data => {
+            res.send(data);
+        }).catch(err => {
+            req.status(500).send({ message: err.message } || `Failed to get foodcalendar with id: ${id}`);
+        });
 
 }
--- server.js
+++ server.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const helmet = require('helmet');
 const app = express();
 
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(require('connect').bodyParser());
 
 var corsOptions = {
-    origin: 'http://localhost:8081'
+    origin: 'http://localhost:3000'
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+
+app.use(function(req,res,next) {
+    console.log("Time: %d", Date.now());
+    next();
+});
 
 require("./app/routes/fooditems.routes")(app);
 require("./app/routes/foodcalendar.routes")(app);
 
 const db = require("./app/models");
+const { TIME } = require('sequelize');
 db.sequelize
     //.sync()
     .authenticate()
